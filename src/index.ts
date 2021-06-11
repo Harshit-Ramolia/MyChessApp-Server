c1738ce4764467cd05f876ac4e0eeb939da67f81
Harshit-Ramolia <ramolia.mh@iitgn.ac.in>
2021-06-12 00:02:10 +0530

    session configration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import routes from "./routes";
 import "./config/mongoose";
+import cors from "cors";
+import session from "express-session";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(express.json());
+app.use(cors({ origin: "*", credentials: true }));
+app.use(session({ secret: "secret", resave: true, saveUninitialized: true }));
 app.use(routes);
 
 app.listen(PORT, () => {
