bce42c031be41b3a21fe055c3d01c329bd37458b
Harshit-Ramolia <ramolia.mh@iitgn.ac.in>
2021-06-28 04:00:04 +0530

    Drop files from .gitignore

--- dist/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Resolvers = void 0;
-const user_1 = require("./user");
-const chess_1 = require("./chess");
-const invitation_1 = require("./invitation");
-exports.Resolvers = [user_1.UserResolver, chess_1.ChessResolver, invitation_1.InvitationResolver];
-//# sourceMappingURL=index.js.map
